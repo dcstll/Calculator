d9eb6a5bd790bf9f311630c3748609e2b26c4b7f
dcstll <danicastillo789@gmail.com>
2022-11-28 18:27:03 -0500

    Trabajo completo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const display = document.querySelector('#display')
-const buttons = document.querySelectorAll('button')
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
 
 buttons.forEach((item) => {
     item.onclick = () => {
@@ -17,5 +17,5 @@ buttons.forEach((item) => {
             display.innerText += item.id;
         }
     }
-}
-)
+})
+
